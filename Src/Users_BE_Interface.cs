b6d782e97fe9e34e28959cb4c9b5ad214ab62e59
GuhankarthickC <77534563+GuhankarthickC@users.noreply.github.com>
2022-06-02 15:53:48 +0530

    Add files via upload

---
+++ Src/Users_BE_Interface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Front_office_staff_Backend
+{
+    interface Users_BE_Interface
+    {
+        void ViewDoctors();
+        void AddPatient();
+        void ScheduleAppointment();
+        void CancelAppointment();
+        void Homepage();
+        bool Login();
+    }
+}
